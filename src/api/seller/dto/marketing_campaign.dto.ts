3fee0651f4483fe40ad55091b542492b5b92f905
Alexey Zamasskin <zamasskin@yandex.ru>
2021-10-01 10:55:22 +0300

    Сделал связь свойств и добавил параметры

--- src/api/seller/dto/marketing_campaign.dto.ts
+++ src/api/seller/dto/marketing_campaign.dto.ts
@@ -1,7 +1,8 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsInt } from 'class-validator';
+import { MarketingCampaignParamsDto } from 'src/marketing-campaign/dto/marketing-campaign-params.dto';
 
-export class GetMarketingCampaignDto {
+export class GetMarketingCampaignDto implements MarketingCampaignParamsDto {
   @ApiProperty()
   @IsInt()
   readonly guestId: number;
@@ -9,4 +10,12 @@ export class GetMarketingCampaignDto {
   @ApiProperty()
   @IsInt()
   readonly userId: number = 0;
+
+  @ApiProperty()
+  @IsInt()
+  readonly countryId: number = 134;
+
+  @ApiProperty()
+  @IsInt()
+  readonly langId: number = 1;
 }
--- src/marketing-campaign/dto/marketing-campaign-params.dto.ts
+++ src/marketing-campaign/dto/marketing-campaign-params.dto.ts
@@ -6,4 +6,10 @@ export class MarketingCampaignParamsDto {
 
   @IsInt()
   readonly userId: number = 0;
+
+  @IsInt()
+  readonly countryId: number = 134;
+
+  @IsInt()
+  readonly langId: number = 1;
 }
