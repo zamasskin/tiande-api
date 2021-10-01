52116aa7d31adc7701e49ef123ddd69bfcc236f4
Alexey Zamasskin <zamasskin@yandex.ru>
2021-10-01 12:28:31 +0300

    Поменял пути

--- src/configurations/configurations.module.ts
+++ src/configurations/configurations.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import configuration from 'src/config/configuration';
+import configuration from '../config/configuration';
 import { LangService } from './lang/lang.service';
 import { CountryService } from './country/country.service';
 
