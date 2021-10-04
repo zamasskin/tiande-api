6cfafcac13705d9edbb8f9ab266e55ce2ce8e36e
Alexey Zamasskin <zamasskin@yandex.ru>
2021-10-04 13:41:37 +0300

    Запрос построителя пути по разделу

--- src/iblock/element/element.service.ts
+++ src/iblock/element/element.service.ts
@@ -2,12 +2,16 @@ import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { plainToClass } from 'class-transformer';
 import { Knex } from 'knex';
+import { SectionService } from '../section/section.service';
 import { ElementModel } from './models/element.model';
 
 @Injectable()
 export class ElementService {
   qb: Knex;
-  constructor(configService: ConfigService) {
+  constructor(
+    configService: ConfigService,
+    private sectionService: SectionService,
+  ) {
     this.qb = configService.get('knex');
   }
 
@@ -16,5 +20,20 @@ export class ElementService {
       ElementModel,
       await this.qb('b_iblock_element').whereIn('ID', id),
     );
+  }
+
+  async findSectionCodePathsById(id: []): Promise<(number) => string> {
+    const elements = await this.findElementsRawById(id);
+    const sectionId = elements
+      .filter((e) => e.sectionId)
+      .map((e) => e.sectionId);
+    if (sectionId.length === 0) {
+      return () => '';
+    }
+    const paths = await this.sectionService.findSectionCodePaths(sectionId);
+    return (id: number) => {
+      const path = paths.find((p) => p.id === id);
+      return path?.path || '';
+    };
   }
 }
