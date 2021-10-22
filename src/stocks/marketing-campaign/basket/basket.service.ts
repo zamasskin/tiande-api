bda982e69c4eb912c1764afcc17fb151c282983a
Alexey Zamasskin <zamasskin@yandex.ru>
2021-10-22 14:18:05 +0300

    Функции для активации и деактивации корзин

--- src/stocks/marketing-campaign/basket/basket.service.ts
+++ src/stocks/marketing-campaign/basket/basket.service.ts
@@ -4,6 +4,7 @@ import { Knex } from 'knex';
 import { ProductService } from 'src/catalog/product/product.service';
 import { BasketService as SaleBasketService } from 'src/sale/basket/basket.service';
 import { BasketEntity } from 'src/sale/basket/entities/basket.entity';
+import { MarketingCampaignEntity } from '../entities/marketing-campaign.entity';
 import { MarketingCampaignService } from '../marketing-campaign.service';
 import { MCBasketParamsDto } from './dto/mc-basket-params.dto';
 
@@ -67,5 +68,67 @@ export class BasketService {
     basketData.oldPrice = basketData.price;
     basketData.price = stock.calculate(basketData.price);
     return basketData;
+  }
+
+  async activateCanBay(
+    basketList: BasketEntity[],
+    stocks: MarketingCampaignEntity[],
+  ) {
+    const basketListForActivate = basketList.filter(
+      (b) => b.canBay === false && b.marketingCampaignId > 0,
+    );
+    if (basketListForActivate.length === 0) {
+      return [];
+    }
+    const productsId = basketListForActivate.map((b) => b.productId);
+    const productOffers = await this.productService.findProductsByOfferId(
+      productsId,
+    );
+    const activateBasketList = basketListForActivate.filter((basket) => {
+      const { productId } = productOffers.find(
+        (po) => po.offerId === basket.productId,
+      );
+
+      return !!stocks.find(
+        (s) =>
+          s.groupInfo.id === basket.marketingCampaignId && s.id === productId,
+      );
+    });
+    const activateId = activateBasketList.map((b) => b.id);
+    if (activateId.length > 0) {
+      await this.basketService.setCanBayById(true, activateId);
+    }
+    return activateId;
+  }
+
+  async deactivateCanBay(
+    basketList: BasketEntity[],
+    stocks: MarketingCampaignEntity[],
+  ) {
+    const basketListForActivate = basketList.filter(
+      (b) => b.canBay === true && b.marketingCampaignId > 0,
+    );
+    if (basketListForActivate.length === 0) {
+      return [];
+    }
+    const productsId = basketListForActivate.map((b) => b.productId);
+    const productOffers = await this.productService.findProductsByOfferId(
+      productsId,
+    );
+    const deactivateBasketList = basketListForActivate.filter((basket) => {
+      const { productId } = productOffers.find(
+        (po) => po.offerId === basket.productId,
+      );
+
+      return !stocks.find(
+        (s) =>
+          s.groupInfo.id === basket.marketingCampaignId && s.id === productId,
+      );
+    });
+    const deactivateId = deactivateBasketList.map((b) => b.id);
+    if (deactivateId.length > 0) {
+      await this.basketService.setCanBayById(false, deactivateId);
+    }
+    return deactivateId;
   }
 }
