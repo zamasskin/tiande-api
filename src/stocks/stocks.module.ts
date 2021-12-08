246faf85cda7c3bcb653b6117a4535afd486e715
Alexey Zamasskin <zamasskin@yandex.ru>
2021-12-08 14:40:46 +0300

    Нужно было сделать через сервисы

--- src/stocks/stocks.module.ts
+++ src/stocks/stocks.module.ts
@@ -1,11 +1,38 @@
 import { Module } from '@nestjs/common';
-import { MarketingCampaignModule } from './marketing-campaign/marketing-campaign.module';
-import { GiftModule } from './gift/gift.module';
 import { StocksService } from './stocks.service';
+import { MarketingCampaignService } from './marketing-campaign/marketing-campaign.service';
+import { MCBasketService } from './marketing-campaign/basket/basket.service';
+import { ConfigModule } from '@nestjs/config';
+import configuration from '../config/configuration';
+import { GiftService } from './gift/gift.service';
+import { CatalogModule } from 'src/catalog/catalog.module';
+import { IblockModule } from 'src/iblock/iblock.module';
+import { ConfigurationsModule } from 'src/configurations/configurations.module';
+import { BasketModule } from 'src/sale/basket/basket.module';
+import { MainModule } from 'src/main/main.module';
 
 @Module({
-  imports: [MarketingCampaignModule, GiftModule],
-  exports: [MarketingCampaignModule, GiftModule, StocksService],
-  providers: [StocksService],
+  imports: [
+    ConfigModule.forRoot({
+      load: [configuration],
+    }),
+    CatalogModule,
+    IblockModule,
+    ConfigurationsModule,
+    BasketModule,
+    MainModule,
+  ],
+  exports: [
+    StocksService,
+    MarketingCampaignService,
+    MCBasketService,
+    GiftService,
+  ],
+  providers: [
+    StocksService,
+    MarketingCampaignService,
+    MCBasketService,
+    GiftService,
+  ],
 })
 export class StocksModule {}
